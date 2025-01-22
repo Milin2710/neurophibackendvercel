89bdce8619b2275f15bc861290aec4699d30603f
Milin2710 <153611524+Milin2710@users.noreply.github.com>
2025-01-22 17:53:23 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -330,7 +330,7 @@ app.use((err, req, res, next) => {
 
 // Your error handler and port configuration remain the same
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://neurophi.tech"); // Set your frontend domain
+  res.header("Access-Control-Allow-Origin", "*"); // Set your frontend domain
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();

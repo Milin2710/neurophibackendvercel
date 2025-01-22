ccb00d959df7ef58e6b4306fc43ca535125666fc
Milin2710 <153611524+Milin2710@users.noreply.github.com>
2025-01-22 16:57:32 +0530

    opt index.js

--- api/index.js
+++ api/index.js
@@ -331,5 +331,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.options('*', cors({
+  origin: 'https://neurophi.tech',
+  credentials: true,
+}));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

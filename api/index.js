cd26d5bc53ba65f55bfde1d565d156efd1551891
Milin2710 <153611524+Milin2710@users.noreply.github.com>
2025-01-23 13:05:52 +0530

    localll index.js

--- api/index.js
+++ api/index.js
@@ -330,7 +330,7 @@ app.use((err, req, res, next) => {
 
 // Your error handler and port configuration remain the same
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
